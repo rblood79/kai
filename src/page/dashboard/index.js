41508cecf47fcc9a33461c71fc9f876ac34b2f53
rblood79 <rblood79@gmail.com>
2022-07-20 15:10:39 +0900

    aa

--- src/page/dashboard/index.js
+++ src/page/dashboard/index.js
@@ -16,30 +16,30 @@ const App = () => {
                 <div className={classNames(styles.body)}>contents</div>
             </section>
 
-            {/*<section className={classNames(styles.section)}>
+            <section className={classNames(styles.section)}>
                 <div className={classNames(styles.header)}>
                     <h2 className={classNames(styles.title)}>Flight No</h2>
                 </div>
-               <div className={classNames(styles.body)}>contents</div>
+                <div className={classNames(styles.body)}>contents</div>
             </section>
             <section className={classNames(styles.section)}>
                 <div className={classNames(styles.header)}>
                     <h2 className={classNames(styles.title)}>Defect</h2>
                 </div>
-               <div className={classNames(styles.body)}>contents</div>
+                <div className={classNames(styles.body)}>contents</div>
             </section>
             <section className={classNames(styles.section)}>
                 <div className={classNames(styles.header)}>
                     <h2 className={classNames(styles.title)}>Maintenance</h2>
                 </div>
-               <div className={classNames(styles.body)}>contents</div>
+                <div className={classNames(styles.body)}>contents</div>
             </section>
             <section className={classNames(styles.section)}>
                 <div className={classNames(styles.header)}>
                     <h2 className={classNames(styles.title)}>Consume</h2>
                 </div>
-               <div className={classNames(styles.body)}>contents</div>
-    </section>*/}
+                <div className={classNames(styles.body)}>contents</div>
+            </section>
         </main>
     );
 }
