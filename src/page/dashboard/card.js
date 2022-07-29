b88339ea31bb721816ea84059e902102eeef6ec8
rblood79 <rblood79@gmail.com>
2022-07-30 02:57:31 +0900

    asd

--- src/page/dashboard/card.js
+++ src/page/dashboard/card.js
@@ -45,36 +45,37 @@ const App = () => {
     });
     const [type, setType] = useState('list');
     const [currentIndex, setCurrentIndex] = useState(0);
-    /*
-
-    const [{ x, y, scale }, api] = useSpring(() => ({ x: 36, y: 0, scale: 1 }));
-
-    const bind = useGesture({
-        onDrag: ({ active, movement: [mx, my], direction: [xDir], cancel }) => {
-            if (active && Math.abs(mx) > (windowDimenion.windWidth - 72) * 0.5) {
-                index.current = clamp(index.current + (xDir > 0 ? -1 : 1), 0, 5);
-                cancel()
-            }
-            api.start({
-                x: index.current * -1 * (windowDimenion.windWidth - 72) + (active ? mx + 36 : 36),
-                y: active ? my : 0,
-                scale: active ? 1 - Math.abs(mx) / windowDimenion.windWidth / 2 : 1
-            })
-
-            setCurrentIndex(index.current);
-        }
-    }, {
-        drag: { axis: 'lock' }
-    });*/
     const index = useRef(0)
     const width = window.innerWidth - 96
 
     const [props, api] = useSprings(data.length, i => ({
         x: (i * width) + 48,
         scale: i === 0 ? 1 : 0.8,
-        //display: 'block',
+        y: 0,
+        //display: 'flex',
     }))
-    const bind = useDrag(({ active, movement: [mx], direction: [xDir], cancel }) => {
+    //const [{ x, y, scale }, api] = useSpring(() => ({ x: 36, y: 0, scale: 1 }));
+
+    const bind = useGesture({
+        onDrag: ({ active, movement: [mx, my], direction: [xDir], cancel }) => {
+            if (active && Math.abs(mx) > width / 2) {
+                index.current = clamp(index.current + (xDir > 0 ? -1 : 1), 0, data.length - 1)
+                cancel()
+            }
+            api.start(i => {
+                if (i < index.current - 1 || i > index.current + 1) return { display: 'none' }
+                const x = (i - index.current) * width + (active ? mx + 48 : 48)
+                const y = active ? my : 0
+                const scale = i === index.current ? 1 : 0.8
+                setCurrentIndex(index.current)
+                return { x, y, scale, display: 'flex' }
+            })
+        }
+    }, {
+        drag: { axis: 'lock' }
+    });
+
+    /*const bind = useDrag(({ active, movement: [mx], direction: [xDir], cancel }) => {
         if (active && Math.abs(mx) > width / 2) {
             index.current = clamp(index.current + (xDir > 0 ? -1 : 1), 0, data.length - 1)
             cancel()
@@ -86,20 +87,19 @@ const App = () => {
             setCurrentIndex(index.current)
             return { x, scale, display: 'flex' }
         })
-    })
+    })*/
     return (
-        <section className={classNames(styles.container)} style={{ height: windowDimenion.windHeight - 118 }}>
+        <section className={classNames(styles.container)}>
             <div className={styles.controller}>
                 <div className={classNames(styles.type, type === 'list' && styles.active)}>
                     <button className={classNames(type === 'list' && styles.active)} onClick={() => { setType('list') }}>LIST</button>
                     <button className={classNames(type === 'grid' && styles.active)} onClick={() => { setType('grid') }}>GRID</button>
                 </div>
             </div>
-            {currentIndex}
             <div className={styles.contents}>
-                {props.map(({ x, display, scale }, i) => (
-                    <animated.div className={classNames(styles.item)} {...bind()} key={i} style={{ display, x, scale }}>
-                        <h3 className={styles.title}>{data[i].title}</h3>
+                {props.map(({ x, y, display, scale }, i) => (
+                    <animated.div className={classNames(styles.item)} {...bind()} key={i} style={{ display, x, y, scale }}>
+                        <h3 className={styles.title}>{data[i].title}{currentIndex}</h3>
                         <div className={styles.itemBody}>{data[i].info}</div>
                     </animated.div>
                 ))}
