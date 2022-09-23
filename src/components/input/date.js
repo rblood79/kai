912df3edf64d836416a9d31f0b0923ecb9d7dd31
rblood79 <rblood79@gmail.com>
2022-09-23 17:05:55 +0900

    159

--- src/components/input/date.js
+++ src/components/input/date.js
@@ -55,7 +55,6 @@ const App = (props) => {
 
         setYear(temp)
         setMonth(['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'])
-        //setDay(min)
     }, [])
 
     return (
