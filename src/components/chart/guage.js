6d888bd118ac233de11c848db600ccd977ef46ea
rblood79 <rblood79@gmail.com>
2022-12-05 09:44:19 +0900

    chart bug fix

--- src/components/chart/guage.js
+++ src/components/chart/guage.js
@@ -101,131 +101,130 @@ const App = (props) => {
 
     useEffect(() => {
         const svg = d3.select(svgRef.current);
-        //console.log(svg.clientWidth)
-        //svg.select('g').remove();
-        const width = svgRef.current.clientWidth;
-        const height = svgRef.current.clientHeight;
-        const percent = props.data / 100;
-        const barWidth = props.barWidth;
-        const inner = 16;
-        const radius = width / 2; //Math.min(width, height) / 2;
-        const pi = Math.PI;
-        const piePercent = pi * percent;
-        const range = 180;
-        const colorScale = d3.scaleSequential(d3.interpolateHslLong("#FF5A03", "#0F4DD8")).domain([0, range]);
-        //const colorScale = d3.scaleSequential().interpolator(d3.interpolateSpectral).domain([0,range]);
-        //const colorScale = d3.scaleQuantize().domain([0, range]).range(['#e15759', '#76b7b2', '#4e79a7']);
+        if (svgRef.current.clientWidth > 0) {
 
-        const perToColor = colorScale((180 / 100) * props.data);
+            const width = svgRef.current.clientWidth;
+            const height = svgRef.current.clientHeight;
+            const percent = props.data / 100;
+            const barWidth = props.barWidth;
+            const inner = 16;
+            const radius = width / 2; //Math.min(width, height) / 2;
+            const pi = Math.PI;
+            const piePercent = pi * percent;
+            const range = 180;
+            const colorScale = d3.scaleSequential(d3.interpolateHslLong("#FF5A03", "#0F4DD8")).domain([0, range]);
+            //const colorScale = d3.scaleSequential().interpolator(d3.interpolateSpectral).domain([0,range]);
+            //const colorScale = d3.scaleQuantize().domain([0, range]).range(['#e15759', '#76b7b2', '#4e79a7']);
 
-        let chart;
-        if (svg.selectAll('.chart')._groups[0].length > 0) {
-            chart = svg.select('.chart')
-        } else {
-            chart = svg.append('g').attr('class', 'chart').attr('transform', "translate(" + ((width + 0) / 2) + ", " + ((height - 24) / 1) + ")")
-        };
+            const perToColor = colorScale((180 / 100) * props.data);
 
-        const arcMin = -pi / 2;
-        const arcMax = pi / 2;
-        const dataDomain = [0, 50, 100];
-        const arcScale = d3.scaleLinear().domain(dataDomain).range([arcMin, 0, arcMax]);
-
-
-
-        if (!chart.selectAll('.arcBase')._groups[0].length > 0) {
-            const arcBase = d3.arc()
-                .outerRadius(radius)
-                .innerRadius(radius - inner - barWidth - 16)
-                .startAngle(arcMin)
-                .endAngle(arcMax)
-
-            chart
-                .append('path')
-                .style('fill', '#383838')
-                .attr("class", "arcBase")
-                .attr('d', arcBase);
-        };
-
-        if (!svg.selectAll('.gradient')._groups[0].length > 0) {
-            const arc = d3.arc()
-                .innerRadius(radius - inner - barWidth)
-                .outerRadius(radius - inner)
-                .startAngle(function (d) { return d.startAngle; })
-                .endAngle(function (d) { return d.endAngle; });
-
-            const data = d3.range(range - 1).map(function (d, i) {
-                return {
-                    startAngle: arcMin + (i * (pi / range)),
-                    endAngle: arcMin + ((i + 2) * (pi / range)),
-                    fill: colorScale(d)
-                };
-            });
-
-            chart.append('g').attr("class", "gradient")
-                .selectAll('path').data(data).enter().append('path').attr("d", arc)
-                .attr("fill", (d) => d.fill)
-        }
-
-        const arcTween = (newAngle) => {
-            return (d) => {
-                var interpolate = d3.interpolate(d.endAngle, newAngle);
-                return (t) => {
-                    d.endAngle = interpolate(t);
-                    return arcValue(d);
-                };
+            let chart;
+            if (svg.selectAll('.chart')._groups[0].length > 0) {
+                chart = svg.select('.chart')
+            } else {
+                chart = svg.append('g').attr('class', 'chart').attr('transform', "translate(" + ((width + 0) / 2) + ", " + ((height - 24) / 1) + ")")
             };
+
+            const arcMin = -pi / 2;
+            const arcMax = pi / 2;
+            const dataDomain = [0, 50, 100];
+            const arcScale = d3.scaleLinear().domain(dataDomain).range([arcMin, 0, arcMax]);
+
+
+            if (!chart.selectAll('.arcBase')._groups[0].length > 0) {
+                const arcBase = d3.arc()
+                    .outerRadius(radius)
+                    .innerRadius(radius - inner - barWidth - 16)
+                    .startAngle(arcMin)
+                    .endAngle(arcMax)
+
+                chart
+                    .append('path')
+                    .style('fill', '#383838')
+                    .attr("class", "arcBase")
+                    .attr('d', arcBase);
+            };
+
+            if (!svg.selectAll('.gradient')._groups[0].length > 0) {
+                const arc = d3.arc()
+                    .innerRadius(radius - inner - barWidth)
+                    .outerRadius(radius - inner)
+                    .startAngle(function (d) { return d.startAngle; })
+                    .endAngle(function (d) { return d.endAngle; });
+
+                const data = d3.range(range - 1).map(function (d, i) {
+                    return {
+                        startAngle: arcMin + (i * (pi / range)),
+                        endAngle: arcMin + ((i + 2) * (pi / range)),
+                        fill: colorScale(d)
+                    };
+                });
+
+                chart.append('g').attr("class", "gradient")
+                    .selectAll('path').data(data).enter().append('path').attr("d", arc)
+                    .attr("fill", (d) => d.fill)
+            }
+
+            const arcTween = (newAngle) => {
+                return (d) => {
+                    var interpolate = d3.interpolate(d.endAngle, newAngle);
+                    return (t) => {
+                        d.endAngle = interpolate(t);
+                        return arcValue(d);
+                    };
+                };
+            }
+
+            chart.selectAll(".ticks").data(arcScale.ticks(1))
+                .enter().append("text")
+                .attr("class", "ticks")
+                .style("fill", "#b4b4b4")
+                .style("font-size", "12px")
+                .style("font-family", "Digital Numbers Regular")
+                .style("text-anchor", "middle")
+                .text(function (d) { return d; });
+
+            chart.selectAll(".ticks")
+                .attr("x", function (d) {
+                    var xVal = Math.cos(arcScale(d) + arcMin) * (radius - 6);
+                    return d === 0 ? xVal - 3 : d === 100 ? xVal - 7 : xVal;
+                })
+                .attr("y", function (d) {
+                    var yVal = Math.sin(arcScale(d) + arcMin) * (radius - 12);
+                    return yVal < -1 ? yVal : + 12;
+                })
+
+            //
+            const arcValue = d3.arc()
+                .outerRadius(radius - inner - 16)
+                .innerRadius(0)
+                .startAngle(-pi / 2)
+
+            let foreground;
+
+            if (!chart.selectAll('.foreground')._groups[0].length > 0) {
+                foreground = chart.append("path")
+                    .datum({ endAngle: -pi / 2, })
+                    .style("fill", "#FF5A03")
+                    .attr("class", "foreground")
+                    .attr("d", arcValue);
+            } else {
+                foreground = chart.select('.foreground')
+            };
+
+            const needle = new Needle((width * 0.5) - barWidth, 6);
+            needle.drawOn(chart, 0);
+            needle.animateOn(chart, props.active ? percent : 0);
+
+            foreground.transition()
+                .delay(props.active ? delay : 0)
+                .duration(props.active ? duration : 0)
+                .ease(easing)
+                .attrTween("d", arcTween(arcMin + (props.active ? piePercent : 0)))
+                .style("fill", perToColor);
+
+            //props.active && needle.animateOn(chart, percent);
         }
-
-        chart.selectAll(".ticks").data(arcScale.ticks(1))
-            .enter().append("text")
-            .attr("class", "ticks")
-            .style("fill", "#b4b4b4")
-            .style("font-size", "12px")
-            .style("font-family", "Digital Numbers Regular")
-            .style("text-anchor", "middle")
-            .text(function (d) { return d; });
-
-        chart.selectAll(".ticks")
-            .attr("x", function (d) {
-                var xVal = Math.cos(arcScale(d) + arcMin) * (radius - 6);
-                return d === 0 ? xVal - 3 : d === 100 ? xVal - 7 : xVal;
-            })
-            .attr("y", function (d) {
-                var yVal = Math.sin(arcScale(d) + arcMin) * (radius - 12);
-                return yVal < -1 ? yVal : + 12;
-            })
-
-        //
-        const arcValue = d3.arc()
-            .outerRadius(radius - inner - 16)
-            .innerRadius(0)
-            .startAngle(-pi / 2)
-
-        let foreground;
-
-        if (!chart.selectAll('.foreground')._groups[0].length > 0) {
-            foreground = chart.append("path")
-                .datum({ endAngle: -pi / 2, })
-                .style("fill", "#FF5A03")
-                .attr("class", "foreground")
-                .attr("d", arcValue);
-        } else {
-            foreground = chart.select('.foreground')
-        };
-
-        const needle = new Needle((width * 0.5) - barWidth, 6);
-        needle.drawOn(chart, 0);
-        needle.animateOn(chart, props.active ? percent : 0);
-
-        foreground.transition()
-            .delay(props.active ? delay : 0)
-            .duration(props.active ? duration : 0)
-            .ease(easing)
-            .attrTween("d", arcTween(arcMin + (props.active ? piePercent : 0)))
-            .style("fill", perToColor);
-
-        //props.active && needle.animateOn(chart, percent);
-
     }, [props.active]);
 
     return (
